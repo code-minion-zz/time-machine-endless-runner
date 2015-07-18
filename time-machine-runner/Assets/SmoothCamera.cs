ccb6e8443626f264e32ff084528d43a8b838351f
Kit <kit@cerebralfix.com>
2015-07-18 14:30:04 +1200

    smooth follow

---
+++ time-machine-runner/Assets/SmoothCamera.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmoothCamera : MonoBehaviour {
+    
+    public Transform target;
+    public float dampTime = 0.15f;
+	private Vector3 velocity = Vector3.zero;
+	Camera _camera;
+
+	void Start() 
+	{
+		_camera = Camera.main;
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (target)
+		{
+			Vector3 point = Camera.main.WorldToViewportPoint(target.position);
+			Vector3 delta = target.position - _camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
+			Vector3 destination = transform.position + delta;
+			transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+		}
+		
+	}
+}
