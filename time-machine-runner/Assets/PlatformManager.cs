b7b2566da0ae614dbfcdc0484bc80afb39c4e579
Alex Gray <alex.gray@cerebralfix.com>
2015-07-18 14:16:18 +1200

    save scene

---
+++ time-machine-runner/Assets/PlatformManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlatformManager : MonoBehaviour {
+
+	private Queue<GameObject> floors;
+	public GameObject floorPrefab;
+	public GameObject guy;
+
+
+	// Use this for initialization
+	void Start () {
+		floors = new Queue<GameObject> ();
+		for (int i = 0; i < 4; i++) {
+			float xPos = -10 + (floorPrefab.transform.localScale.x * floors.Count);
+			floors.Enqueue(GameObject.Instantiate (floorPrefab, new Vector3(xPos, -4.5f, 0f), new Quaternion()) as GameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+
+}
---
+++ time-machine-runner/Assets/Player.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = new Vector3(this.transform.position.x + (1 * Time.deltaTime), this.transform.position.y, 0);
+	}
+}
