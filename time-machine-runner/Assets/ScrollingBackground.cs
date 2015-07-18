c6cbb71cb34ec4be336b195b30b7c24b34864323
Alex Gray <alex.gray@cerebralfix.com>
2015-07-18 18:03:09 +1200

    Parallax Background

---
+++ time-machine-runner/Assets/ScrollingBackground.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollingBackground : MonoBehaviour {
+
+	public float speed = 0.005f;
+
+	float pos = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		pos += speed;
+		if (pos > 1.0f)
+			pos -= 1.0f;
+		Renderer sr = GetComponent<Renderer> ();
+		sr.material.mainTextureOffset = new Vector2 (pos, 0);
+		Debug.Log (pos);
+	}
+
+
+}
