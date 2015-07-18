df809964f15dde68c89c0ae7aeb0c29de853aa3c
Alex Gray <alex.gray@cerebralfix.com>
2015-07-18 16:26:02 +1200

    Update jumping and displaying distance

--- time-machine-runner/Assets/Player.cs
+++ time-machine-runner/Assets/Player.cs
@@ -1,7 +1,13 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Player : MonoBehaviour {
+
+	bool jumping;
+	bool doubleJumping;
+
+	public Text distanceDisplay;
 
 	// Use this for initialization
 	void Start () {
@@ -10,6 +16,19 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		this.transform.position = new Vector3(this.transform.position.x + (1 * Time.deltaTime), this.transform.position.y, 0);
+		this.transform.position = new Vector3(this.transform.position.x + (10 * Time.deltaTime), this.transform.position.y, 0);
+		if (Input.anyKeyDown && !doubleJumping) {
+			GetComponent<Rigidbody2D>().AddForce(Vector3.up * 300, ForceMode2D.Force);
+			if(jumping)
+				doubleJumping = true;
+			jumping = true;
+		}
+
+		distanceDisplay.text = (int)this.transform.position.x + "m";
+	}
+
+	void OnCollisionEnter2D(Collision2D coll){
+		jumping = false;
+		doubleJumping = false;
 	}
 }
